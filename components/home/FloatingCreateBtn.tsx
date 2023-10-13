e24b6e4b74c12bb3fa111c4c5f7b6a9a5ba1b06e
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-13 05:00:01 +0100

    made component more generic

--- components/home/FloatingCreateBtn.tsx
+++ components/home/FloatingCreateBtn.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Button from "../shared/Button";
 import Link from "next/link";
 
-const FloatingCreateBtn = ({}) => {
+interface IFloatingCreateBtn {
+  fill?: boolean;
+}
+
+const FloatingCreateBtn: React.FC<IFloatingCreateBtn> = ({ fill }) => {
   return (
-    <div className="fixed bottom-0 left-0 flex w-full items-center justify-center gap-4 bg-white py-5 shadow-strong lg:hidden">
+    <div className="fixed bottom-0 z-10 left-0 flex w-full items-center justify-center gap-4 bg-white py-5 shadow-strong lg:hidden">
       <Link href={"/nominate"} className="block w-[85%]">
         <Button
           bg="bg-black"
-          variant="outline"
+          variant={fill ? "solid" : "outline"}
           borderColor="border-black"
           text="CREATE NEW NOMINATION"
-          color="text-black"
+          color={fill ? "text-white" : "text-black"}
           className={` w-full font-primary font-bold`}
         />
       </Link>
