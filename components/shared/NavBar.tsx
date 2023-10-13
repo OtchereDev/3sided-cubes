1b2e35f17af85ed717631f11d46797ff3ed4a6ff
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-13 03:56:23 +0100

    added links to navbar

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -4,24 +4,35 @@ import LogoSm from "@/assets/svgs/logo-sm.svg";
 import LogoLg from "@/assets/svgs/logo-lg.svg";
 import Plus from "@/assets/svgs/plus.svg";
 import Inbox from "@/assets/svgs/inbox.svg";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="fixed left-0  top-0 flex w-full items-center justify-between bg-black px-9 py-6 lg:px-8 lg:py-5">
+    <nav className="fixed left-0  top-0 z-10 flex w-full items-center justify-between bg-black px-9 py-6 lg:px-8 lg:py-5">
       <div>
-        <Image src={LogoSm} alt="logo" className="lg:hidden" />
-        <Image src={LogoLg} alt="logo" className="hidden lg:block" />
+        <Link href={"/"}>
+          <Image src={LogoSm} alt="logo" className="lg:hidden" />
+        </Link>
+        <Link href={"/"}>
+          <Image src={LogoLg} alt="logo" className="hidden lg:block" />
+        </Link>
       </div>
 
       <div className="hidden lg:block">
-        <p className=" cursor-pointer font-secondary text-base font-bold text-white underline">
-          Your Nominations (3)
-        </p>
+        <Link href={"/my-nominations"}>
+          <p className=" cursor-pointer font-secondary text-base font-bold text-white underline">
+            Your Nominations (3)
+          </p>
+        </Link>
       </div>
 
       <div className="flex items-center gap-5 lg:hidden">
-        <Image src={Plus} alt="Add new nomination" />
-        <Image src={Inbox} alt="Your nomination" />
+        <Link href={"/nominate"}>
+          <Image src={Plus} alt="Add new nomination" />
+        </Link>
+        <Link href={"/my-nominations"}>
+          <Image src={Inbox} alt="Your nomination" />
+        </Link>
       </div>
     </nav>
   );
