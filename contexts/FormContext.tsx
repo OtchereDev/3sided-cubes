227c8b8df21c8150ce5905cf6cf4df8afe5242a5
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-14 17:22:55 +0100

    completed form context

--- contexts/FormContext.tsx
+++ contexts/FormContext.tsx
@@ -1,7 +1,13 @@
-import { createContext, useContext } from "react";
+import {
+  BaseSyntheticEvent,
+  createContext,
+  useContext,
+  useEffect,
+} from "react";
 import {
   Control,
   FieldErrors,
+  UseFormHandleSubmit,
   UseFormRegister,
   UseFormSetValue,
   UseFormTrigger,
@@ -9,6 +15,24 @@ import {
 } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
+import {
+  CubeAcademyGetAllNominationsError,
+  useCubeAcademyCreateNomination,
+  useCubeAcademyGetNominationById,
+  useCubeAcademyRetrieveNomineeList,
+  useCubeAcademyUpdateNomination,
+} from "@/api/apiComponents";
+import { AUTHKEY } from "@/constants/jwt";
+import useLinkGeneration from "@/hooks/useLinkGeneration";
+import { Nomination, Nominations } from "@/api/apiResponses";
+import { useNominationContext } from "./NominationContext";
+import { toast } from "sonner";
+import {
+  RefetchOptions,
+  RefetchQueryFilters,
+  QueryObserverResult,
+} from "@tanstack/react-query";
 
 export type FormValues = {
   nominee_id: string;
@@ -22,8 +46,17 @@ export interface IFormContext {
   setValue: UseFormSetValue<FormValues>;
   trigger: UseFormTrigger<FormValues>;
   errors: FieldErrors<FormValues>;
-  submitForm: (data: FormValues) => void;
   register: UseFormRegister<FormValues>;
+  handleSubmit: UseFormHandleSubmit<FormValues, undefined>;
+  onSubmit: (
+    refetch: <TPageData>(
+      options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined,
+    ) => Promise<
+      QueryObserverResult<Nominations, CubeAcademyGetAllNominationsError>
+    >,
+  ) => (e?: BaseSyntheticEvent<object, any, any> | undefined) => Promise<void>;
+  isCreateLoading: boolean;
+  isUpdateLoading: boolean;
 }
 
 export interface IFormContextProvider {
@@ -60,9 +93,96 @@ const FormProvider: React.FC<IFormContextProvider> = ({ children }) => {
     resolver: yupResolver(NomineeSchema, { abortEarly: false }),
   });
 
-  const formValues = watch();
+  // hooks
+  const { mode, id } = useLinkGeneration();
+  const router = useRouter();
 
-  const submitForm = (data: FormValues) => {};
+  // mutations and queries
+  const { data: nomineeList } = useCubeAcademyRetrieveNomineeList({
+    headers: { authorization: `Bearer ${AUTHKEY}` },
+  });
+  const { data: nominationData } = useCubeAcademyGetNominationById(
+    {
+      headers: { authorization: `Bearer ${AUTHKEY}` },
+      pathParams: { nominationId: id as string },
+    },
+    { enabled: mode == "edit" && (id?.length as number) > 0 },
+  );
+  const { isLoading, mutateAsync } = useCubeAcademyCreateNomination();
+  const { isLoading: isUpdateLoading, mutateAsync: updateMutateAsync } =
+    useCubeAcademyUpdateNomination();
+
+  // for creating and updating nomination
+  const onSubmit = (
+    refetch: <TPageData>(
+      options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined,
+    ) => Promise<
+      QueryObserverResult<Nominations, CubeAcademyGetAllNominationsError>
+    >,
+  ) =>
+    handleSubmit(async (data) => {
+      try {
+        let response: Nomination;
+        if (mode == "edit" && id.length) {
+          // update nomination here
+          response = await updateMutateAsync({
+            headers: {
+              authorization: `Bearer ${AUTHKEY}`,
+            },
+            body: {
+              nominee_id: data.nominee_id,
+              reason: data.reason,
+              process: data.process,
+            },
+            pathParams: {
+              nominationId: id,
+            },
+          });
+        } else {
+          // create nomination here
+          response = await mutateAsync({
+            headers: {
+              authorization: `Bearer ${AUTHKEY}`,
+            },
+            body: {
+              nominee_id: data.nominee_id,
+              reason: data.reason,
+              process: data.process,
+            },
+          });
+        }
+
+        if (response.data) {
+          let successUrl = "/submitted";
+          if (mode == "edit") {
+            successUrl += `?mode=edit`;
+          }
+          await refetch();
+          router.push(successUrl);
+        }
+      } catch (error: any) {
+        toast(error.stack.message);
+      }
+    });
+
+  useEffect(() => {
+    if (nominationData?.data) {
+      setValue("nominee_id", nominationData.data.nominee_id as string);
+      setValue("reason", nominationData.data.reason as string);
+      setValue("process", nominationData.data.process as string);
+
+      const nominee = nomineeList?.data?.find(
+        (person) => person.nominee_id == nominationData.data?.nominee_id,
+      );
+
+      if (nominee) {
+        setValue("cubeName", nominee.first_name + " " + nominee.last_name);
+      }
+      trigger();
+    }
+  }, [nominationData]);
+
+  const formValues = watch();
 
   return (
     <FormContext.Provider
@@ -72,8 +192,11 @@ const FormProvider: React.FC<IFormContextProvider> = ({ children }) => {
         setValue,
         trigger,
         errors,
-        submitForm,
         register,
+        handleSubmit,
+        isCreateLoading: isLoading,
+        isUpdateLoading,
+        onSubmit,
       }}
     >
       {children}
