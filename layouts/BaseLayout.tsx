8ceb9047ca7634f9b4e41c98f112a345d54c0b25
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-14 04:00:26 +0100

    added prop to fix design bug on submit page

--- layouts/BaseLayout.tsx
+++ layouts/BaseLayout.tsx
@@ -2,17 +2,22 @@ import Footer from "@/components/shared/Footer";
 import NavBar from "@/components/shared/NavBar";
 import Head from "next/head";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React from "react";
 import Bg from "@/assets/images/lg/bg-l.png";
 
 interface IBaseLayout {
   children: React.ReactNode;
   title: string;
   addBg?: boolean;
+  highBgTop?: boolean;
 }
 
-const BaseLayout: React.FC<IBaseLayout> = ({ children, title, addBg }) => {
+const BaseLayout: React.FC<IBaseLayout> = ({
+  children,
+  title,
+  addBg,
+  highBgTop,
+}) => {
   const pageTitle = `3 sided cube - ${title}`;
 
   return (
@@ -29,7 +34,9 @@ const BaseLayout: React.FC<IBaseLayout> = ({ children, title, addBg }) => {
         <Image
           src={Bg}
           alt="background"
-          className="absolute left-0 top-11  hidden w-full lg:block"
+          className={`absolute left-0 ${
+            highBgTop ? "top-15" : "top-11"
+          }  hidden w-full lg:block`}
         />
         <div className="lg:relative lg:overflow-hidden">{children}</div>
       </main>
