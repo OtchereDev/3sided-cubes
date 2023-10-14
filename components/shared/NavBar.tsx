39bebe505008e4da47981da13bb1e74e1321333f
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-14 17:35:17 +0100

    completed nav bar

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
 import React from "react";
 
 import Link from "next/link";
 import { Inbox, LogoLg, LogoSm, Plus } from "./icons";
+import { useNominationContext } from "@/contexts/NominationContext";
 
 const NavBar = () => {
+  // TODO: add to writing: I assume that the nomination counter is count all nomination whether closed or current
+  const { nominationCount } = useNominationContext();
   return (
     <nav className="fixed left-0  top-0 z-50 flex w-full items-center justify-between bg-black px-9 py-6 lg:px-8 lg:py-5">
       <div>
@@ -19,7 +21,7 @@ const NavBar = () => {
       <div className="hidden lg:block">
         <Link href={"/my-nominations"}>
           <p className=" cursor-pointer font-secondary text-base font-bold text-white underline">
-            Your Nominations (3)
+            Your Nominations ({nominationCount})
           </p>
         </Link>
       </div>
