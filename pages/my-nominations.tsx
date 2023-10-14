6066a6fe405a986722440e6e92712f0370da423b
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-14 17:38:59 +0100

    completed my nomination page

--- pages/my-nominations.tsx
+++ pages/my-nominations.tsx
@@ -1,15 +1,26 @@
 import FloatingCreateBtn from "@/components/form-process/FloatingCreateBtn";
 import Nomination from "@/components/nominations/Nomination";
 import Button from "@/components/shared/Button";
+import { useNominationContext } from "@/contexts/NominationContext";
 import BaseLayout from "@/layouts/BaseLayout";
+import { isClosedNomination } from "@/utils/isClosed";
 import React, { useState } from "react";
 
 const MyNomination = () => {
   const [isClosed, setIsClosed] = useState(false);
+  const { nominations } = useNominationContext();
+  const closedNomination =
+    nominations?.filter((nomination) =>
+      isClosedNomination(nomination.closing_date as string),
+    ) ?? [];
+  const openNomination =
+    nominations?.filter(
+      (nomination) => !isClosedNomination(nomination.closing_date as string),
+    ) ?? [];
   return (
     <BaseLayout addBg title="My nomination">
       <div className="relative lg:mx-auto lg:w-[1216px]">
-        <div className="bg-primaryGradient px-6 py-7 lg:bg-transparent lg:px-0">
+        <div className="bg-primaryGradient px-6 py-7 lg:bg-none lg:px-0">
           <h3 className="mb-3 text-2xl font-bold leading-[38px] lg:text-[32px]">
             YOUR NOMINATIONS
           </h3>
@@ -40,7 +51,7 @@ const MyNomination = () => {
             />
           </div>
         </div>
-        <div className="bg-white lg:mb-20">
+        <div className="bg-white mb-20 lg:mb-20 lg:h-[611px] lg:overflow-scroll">
           <table className="min-w-full divide-y divide-gray-300">
             <thead className="bg-gray-50">
               <tr>
@@ -50,7 +61,7 @@ const MyNomination = () => {
                 >
                   NOMINEE
                 </th>
-                <th scope="col" className="">
+                <th scope="col" className="hidable-table-cell">
                   DATE SUBMITTED
                 </th>
                 <th scope="col" className="hidable-table-cell ">
@@ -69,8 +80,19 @@ const MyNomination = () => {
             </thead>
             <tbody>
               {!isClosed
-                ? [1, 3, 4, 5, 6].map((i) => <Nomination key={i} />)
-                : [1, 3, 4, 5, 6].map((i) => <Nomination isClosed key={i} />)}
+                ? openNomination.map((nomination) => (
+                    <Nomination
+                      key={nomination.nomination_id}
+                      nomination={nomination}
+                    />
+                  ))
+                : closedNomination.map((nomination) => (
+                    <Nomination
+                      isClosed
+                      key={nomination.nomination_id}
+                      nomination={nomination}
+                    />
+                  ))}
             </tbody>
           </table>
         </div>
