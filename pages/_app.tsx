9954b9ee3c25e0406615c3785dbd83d64c0a6e0f
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-14 17:36:45 +0100

    added contexts and query client provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,24 @@ import FormContextProvider from "@/contexts/FormContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import NominationProvider from "@/contexts/NominationContext";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <FormContextProvider>
-        <NextNProgress color="#A0FF1F" />
-        <Component {...pageProps} />
-      </FormContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <FormContextProvider>
+          <NominationProvider>
+            <NextNProgress color="#A0FF1F" />
+            <Component {...pageProps} />
+            <Toaster />
+          </NominationProvider>
+        </FormContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
