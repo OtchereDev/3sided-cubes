f73f66b65aa3a13719648e8e39a70c721a470722
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-15 00:49:21 +0100

    added logout feature

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Inbox, LogoLg, LogoSm, Plus } from "./icons";
 import { useNominationContext } from "@/contexts/NominationContext";
 import FormStepper from "../form-process/FormStepper";
+import { useAuthContext } from "@/contexts/AuthContext";
 
 interface INavBar {
   currentStep?: boolean | number;
@@ -12,6 +13,7 @@ interface INavBar {
 const NavBar: React.FC<INavBar> = ({ currentStep }) => {
   // TODO: add to writing: I assume that the nomination counter is count all nomination whether closed or current
   const { nominationCount } = useNominationContext();
+  const { logout } = useAuthContext();
   return (
     <nav className="fixed left-0 top-0 z-50 w-full  bg-black px-5 py-6 lg:px-8 lg:py-5">
       <div className="flex  items-center justify-between">
@@ -24,12 +26,18 @@ const NavBar: React.FC<INavBar> = ({ currentStep }) => {
           </Link>
         </div>
 
-        <div className="hidden lg:block">
+        <div className="hidden lg:flex lg:gap-3">
           <Link href={"/my-nominations"}>
             <p className=" cursor-pointer font-secondary text-base font-bold text-white underline">
               Your Nominations ({nominationCount})
             </p>
           </Link>
+          <p
+            onClick={logout}
+            className="cursor-pointer font-secondary text-base font-bold text-white hover:text-primary-green hover:underline"
+          >
+            Log out
+          </p>
         </div>
 
         <div className="flex items-center gap-5 lg:hidden">
